b4f5c760f1dba7455d6cdd52125c272abb21059e
PedroHenriqueph7 <pedrohenrizzx7@gmail.com>
2025-08-26 08:51:21 -0300

    init: (chore: Configuração do projeto e estruturação)

---
+++ Gerenciador-tarefas-domesticas/src/main/java/com/pedro_henrique/Gerenciador_tarefas_domesticas/GerenciadorTarefasDomesticasApplication.java
@@ -0,0 +1,13 @@
+package com.pedro_henrique.Gerenciador_tarefas_domesticas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorTarefasDomesticasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorTarefasDomesticasApplication.class, args);
+	}
+
+}
---
+++ Gerenciador-tarefas-domesticas/src/test/java/com/pedro_henrique/Gerenciador_tarefas_domesticas/GerenciadorTarefasDomesticasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedro_henrique.Gerenciador_tarefas_domesticas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorTarefasDomesticasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
